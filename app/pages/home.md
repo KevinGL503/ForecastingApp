892ad6357cba624c78d1479b219d3b60a167ae34
Kevin <abelgarcia612@gmail.com>
2024-05-10 22:06:00 -0500

    app home page

---
+++ app/pages/home.md
@@ -0,0 +1,22 @@
+# Power Prices Forecasting App
+This app attempts to predict prices for the ERCOT power market at different zones, 
+based on historical data.
+
+The predictions are made by machine learning models trained on the historical data
+these models have been trained and called when needed.
+
+
+## Data 
+For now, we attempt to predict these prices based on the following historical data:
+* Demand (load)
+* Wind and Solar power generation
+* Prices for different hub zones
+
+**Other data can can be added and used as we go..**
+
+## Models
+Once we have the data we can start experimenting with models, 
+for now, maybe linear regression or gradient boosting
+
+## Purpose
+Make is easier to get a prediction of the prices for the ERCOT market in one page
--- app/pages/home.py
+++ app/pages/home.py
@@ -4,23 +4,16 @@ import dash_bootstrap_components as dbc
 
 dash.register_page(__name__, path_template="/", name="Home")
 
-
-def get_readme_from_github():
-    import requests
-
-    url = "https://github.com/KevinGL503/ForecastingApp/raw/main/README.md"
-    response = requests.get(url)
-
-    raw = response.text
+def get_home_md():
+    file = open('app/pages/home.md', 'r')
+    raw = file.read()
     raw = raw.split("\n")
     new_raw = []
     for line in raw:
-        if not line.startswith("!["):
-            new_raw.append(line)
+        # if not line.startswith("!["):
+        new_raw.append(line)
     raw = "\n".join(new_raw)
-
     return raw
 
-
-def layout():
-    return dbc.Container([dbc.Card(dcc.Markdown(get_readme_from_github()), body=True)])
+layout = dbc.Container(dcc.Markdown(get_home_md()), 
+                       style={'padding':'30px'})
