b052987e04baa9de5e2915b67889a19608b72525
Kevin <abelgarcia612@gmail.com>
2024-05-23 13:46:21 -0500

    class to manage DB

--- api/updater/db_helpers.py
+++ api/updater/db_helpers.py
@@ -3,38 +3,50 @@ import pandas as pd
 import sqlite3
 import os
 
-def db_exists():
-    """
-    The function checks if a database file exists and creates it if it doesn't.
-    :return: always returns `True`.
-    """
-    if not os.path.exists('api/updater/forecastDB.db'):
-        con = sqlite3.connect('api/updater/forecastDB.db')
-        print('Created DB file')
+class DB():
+    """ DB class to manage the database contains connection and cursor"""
+
+    def __init__(self, db_file_path='api/updater/forecastDB.db'):
+        """
+        The function initializes a connection to a SQLite database using a specified
+        file path.
+        
+        :param db_file_path: parameter is the file path to the SQLite database 
+        file that will be used. By default, it is set `'api/updater/forecastDB.db'`
+        This parameter allows you to specify a custom database file path 
+        """
+        self.con = sqlite3.connect(db_file_path)
+        self.cur = self.con.cursor()
+
+    def db_exists(self):
+        """
+        The function checks if a database file exists and creates it if it doesn't.
+        :return: always returns `True`.
+        """
+        if not os.path.exists('api/updater/forecastDB.db'):
+            con = sqlite3.connect('api/updater/forecastDB.db')
+            print('Created DB file')
+            return True
         return True
-    return True
 
-def make_new_table(name, columns):
-    """
-    The function `make_new_table` creates a new table in a SQLite database with the
-    specified name and columns.
-    
-    :param name: name for the db table
-    :param columns: columns in the table
-    """
-    db_exists()
-    cols = ",".join(str(element) for element in columns)
-    query = f"CREATE TABLE IF NOT EXISTS {name} ({cols})"
-    con = sqlite3.connect('api/updater/forecastDB.db')
-    cur = con.cursor()
-    cur.execute(query)
+    def make_new_table(self, name, columns):
+        """
+        The function `make_new_table` creates a new table in a SQLite database with the
+        specified name and columns.
+        
+        :param name: name for the db table
+        :param columns: columns in the table
+        """
+        cols = ",".join(str(element) for element in columns)
+        query = f"CREATE TABLE IF NOT EXISTS {name} ({cols})"
+        self.cur.execute(query)
 
 
-def update_curr_cond():
-    """
-    The function `update_curr_cond` gets the current conditions and stores them
-    in the database in a table called "conditions"
-    """
-    con = sqlite3.connect('api/updater/forecastDB.db')
-    df = dc.get_today_cond()
-    df.to_sql('conditions', con, index=True, index_label='TS', if_exists='replace')
+    def update_curr_cond(self):
+        """
+        The function `update_curr_cond` gets the current conditions and stores them
+        in the database in a table called "conditions"
+        """
+        df = dc.get_today_cond()
+        df.to_sql('conditions', self.con, index=True, index_label='TS', \
+                  if_exists='replace')
---
+++ api/updater/service.py
@@ -0,0 +1,5 @@
+from db_helpers import DB
+
+if __name__ == "__main__":
+    db = DB()
+    db.update_curr_cond()
