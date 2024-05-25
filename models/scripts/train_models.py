2133120c6c77d5c3670850ddc9fd4242b4b86328
Garrett <gad50@txstate.edu>
2024-05-25 16:36:39 -0500

    fixed attribute error in improve_model func

--- models/scripts/train_models.py
+++ models/scripts/train_models.py
@@ -103,9 +103,13 @@ def improve_model(X_train, y_train, model):
         'max_depth': [None, 10, 20, 30],
         'min_samples_split': [2, 5, 10]
     }
-    grid_search = GridSearchCV(estimator=model, param_grid=param_grid, cv=3, scoring='neg_mean_squared_error')
-    grid_search.fit(X_train, y_train)
-    return grid_search.best_estimator_
+    #Check if the model has these parameters in it
+    if 'n_estimators' not in model.get_params().keys() or 'max_depth' not in model.get_params().keys() or 'min_samples_split' not in model.get_params().keys():
+        return model
+    else:
+        grid_search = GridSearchCV(estimator=model, param_grid=param_grid, cv=3, scoring='neg_mean_squared_error')
+        grid_search.fit(X_train, y_train)
+        return grid_search.best_estimator_
 
 def evaluate_model(model, X_test, y_test):
     """This function evaluates the model using the test data
