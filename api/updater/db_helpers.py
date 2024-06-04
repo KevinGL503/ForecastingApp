4c81f8d9b893ebe17f7ffd7f6841b5cea050345e
Kevin <abelgarcia612@gmail.com>
2024-06-04 18:53:51 -0500

    price json format for api

--- api/updater/db_helpers.py
+++ api/updater/db_helpers.py
@@ -154,3 +154,18 @@ class DB():
         except Exception as e:
             return e.args[0]
         return results
+    
+    def get_curr_prices_json(self):
+        """
+        This function retrieves prices data from the database in JSON format.
+        :return: the current stored prices. If an exception occurs during the execution of the query,
+        the function will return the error message.
+        """
+        try:
+            query = self.cur.execute(f"SELECT * FROM prices")
+            cols = [col[0] for col in query.description]
+            data = query.fetchall()
+            results = [dict(zip(cols, row)) for row in data]
+        except Exception as e:
+            return e.args[0]
+        return results
