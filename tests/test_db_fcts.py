c8e96ab4aa0a580ad62d175eb6eadfc306a0d3d4
Kevin <abelgarcia612@gmail.com>
2024-06-06 22:09:48 -0500

    testing db fcts

--- tests/test_db_fcts.py
+++ tests/test_db_fcts.py
@@ -1,25 +1,8 @@
-import api.updater.day_conds as dc 
+from updater.db_helpers import DB
 import pandas as pd
-import numpy as np
-from datetime import datetime
 
-def test_get_today_predicted_fuel():
-    df = dc.get_today_predicted_fuel()
+def test_get_table_as_df():
+    db = DB()
+    df = db.get_table_as_df('conditions')
     assert isinstance(df, pd.DataFrame)
-    assert df.columns.values.tolist() == ['TS', 'Wind', 'Solar']
-    assert pd.to_datetime(df['TS'].values[0]).day == datetime.now().day
-    assert pd.to_datetime(df['TS'].values[0]).month == datetime.now().month
-
-def test_get_today_predicted_load():
-    df = dc.get_today_predicted_load()
-    assert isinstance(df, pd.DataFrame)
-    assert df.columns.values.tolist() == ['TS', 'Load']
-    assert pd.to_datetime(df['TS'].values[0]).day == datetime.now().day
-    assert pd.to_datetime(df['TS'].values[0]).month == datetime.now().month
-
-def test_get_today_cond():
-    df = dc.get_today_cond()
     assert df.isna().sum().sum() == 0
-    assert np.issubdtype(df.index.values[0].dtype, np.datetime64)
-    assert df.columns.values.tolist() == ['Day', 'Hour', 'Wind', 'Solar', \
-                    'Load', 'Prev_Load', 'Net_Load', 'Total_Renew', 'Month']
---
+++ tests/test_dc_fcts.py
@@ -0,0 +1,25 @@
+import updater.day_conds as dc 
+import pandas as pd
+import numpy as np
+from datetime import datetime
+
+def test_get_today_predicted_fuel():
+    df = dc.get_today_predicted_fuel()
+    assert isinstance(df, pd.DataFrame)
+    assert df.columns.values.tolist() == ['TS', 'Wind', 'Solar']
+    assert pd.to_datetime(df['TS'].values[0]).day == datetime.now().day
+    assert pd.to_datetime(df['TS'].values[0]).month == datetime.now().month
+
+def test_get_today_predicted_load():
+    df = dc.get_today_predicted_load()
+    assert isinstance(df, pd.DataFrame)
+    assert df.columns.values.tolist() == ['TS', 'Load']
+    assert pd.to_datetime(df['TS'].values[0]).day == datetime.now().day
+    assert pd.to_datetime(df['TS'].values[0]).month == datetime.now().month
+
+def test_get_today_cond():
+    df = dc.get_today_cond()
+    assert df.isna().sum().sum() == 0
+    assert np.issubdtype(df.index.values[0].dtype, np.datetime64)
+    assert df.columns.values.tolist() == ['Day', 'Hour', 'Wind', 'Solar', \
+                    'Load', 'Prev_Load', 'Net_Load', 'Total_Renew', 'Month']
