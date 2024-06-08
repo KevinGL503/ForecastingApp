bb565dfe7a6dd1187db678d8c24e56f7b5073819
Kevin <abelgarcia612@gmail.com>
2024-06-07 19:04:26 -0500

    archive page in app

--- app/pages/archive.py
+++ app/pages/archive.py
@@ -1,79 +1,53 @@
 import dash
 from dash import html, dash_table, dcc
-import requests
-import pandas as pd
 import dash_bootstrap_components as dbc
+from dash import Input, Output, State, callback
+import app.utils.graphs as gp
 
 
 dash.register_page(__name__)
-df = requests.get(f'http://localhost:5555/prices')
-df = pd.DataFrame(df.json())
-prices = dash_table.DataTable(
-    data=df.to_dict('records'),
-    columns=[{"name": i, "id": i} for i in df.columns],
-    style_as_list_view=True,
-    style_cell={'padding': '5px'},
-    page_action='none',
-    style_table={'height': '800px','overflowY': 'auto'},
-    style_header={
-        'backgroundColor': 'white',
-        'fontWeight': 'bold',
-        'textAlign': 'center'
-    },
-    )
-df = requests.get(f'http://localhost:5555/conditions')
-df = pd.DataFrame(df.json())
-df = df[['TS', 'Load', 'Wind', 'Solar']]
-conds = dash_table.DataTable(
-    data=df.to_dict('records'),
-    columns=[{"name": i, "id": i} for i in df.columns],
-    style_as_list_view=True,
-    style_cell={'padding': '5px'},
-    page_action='none',
-    style_table={'height': '800px','overflowY': 'auto'},
-    style_header={
-        'backgroundColor': 'white',
-        'fontWeight': 'bold',
-        'textAlign': 'center'
-    },
-    )
 
-layout = dbc.Container([html.H3('This is the archive containing data for current day conditions and prices'),
-    html.Div(
-        [html.Div([
-        html.H2('Prices'),
-        prices],
-        style={'padding':'30px',
-            'display': 'flex',
-            'flex-direction': 'column', 
-            'justify-content:':'center',
-            'align-items':'center',
-            #   'column-gap': '100px',
-            }),
-        html.Div([
-        html.H2('Conditions'),
-        conds],
-        style={'padding':'30px',
-            'display': 'flex',
-            'flex-direction': 'column', 
-            'justify-content:':'center',
-            'align-items':'center',
-            #   'column-gap': '100px',
-            }),
-        ], 
-        style={
-            'display': 'flex',
-            'flex-direction': 'row', 
-            'justify-content:':'center',
-            'align-items':'center',
-            'flex-wrap': 'wrap',
-            # 'flex-grow': '1'
-            }
-    )], 
+layout = dbc.Container(
+    [html.H2('Current data for prices and conditions'),
+     dcc.Dropdown(['Prices','Conditions'], 
+                    id='data_selector',
+                    multi=True, style={'width':'50vw'}),
+    html.Div([
+        html.Div(id='table_container', 
+                 style={
+                'display': 'flex',
+                'flex-direction': 'row', 
+                'justify-content:':'center',
+                'align-items':'center',
+                'flex-wrap': 'wrap',
+                'max-width':'100vw',
+                'gap':'30px',
+                'padding': '30px'
+                })
+    ])
+    ], 
     style={'padding':'30px',
             'display': 'flex',
             'flex-direction': 'column', 
             'justify-content:':'center',
             'align-items':'center',
             'max-width':'100vw'
-            })
+            }
+    )
+
+@callback(
+    Output('table_container', 'children'),
+    [Input('data_selector', 'value')],
+    [State('table_container','children')]
+)
+def update_output(selection, curr_children):
+    if not selection:
+        return 
+
+    new_children = []
+    for value in selection:
+        fig = gp.get_table(value)
+        block = html.Div([html.H3(value), fig], id=f'table-{value}')
+        new_children.append(block)
+        
+    return new_children
--- app/utils/graphs.py
+++ app/utils/graphs.py
@@ -2,6 +2,7 @@ import pickle
 import plotly.express as px
 import requests
 import pandas as pd
+from dash import dash_table
 from dotenv import load_dotenv
 import os
 
@@ -51,3 +52,33 @@ def get_graph(region):
     ))
 
     return fig
+
+def get_table(type):
+    """
+    The function `get_table` retrieves data from a specified API endpoint, formats
+    it into a DataFrame, and creates a Dash DataTable with specific styling and
+    columns based on the type provided.
+    
+    :param type: The `type` parameter in the `get_table` function is used to specify
+    the type of data to retrieve from the API. 
+    :return: A Dash DataTable object displaying data from the specified type
+    """
+    df = requests.get(f'{BASE_URL}/api/{type.lower()}')
+    df = pd.DataFrame(df.json())
+    if type == 'Conditions': df = df[['TS', 'Load', 'Wind', 'Solar']]
+    ts = df.pop('TS') 
+    df.insert(0, 'TS', ts) 
+    prices = dash_table.DataTable(
+        data=df.to_dict('records'),
+        columns=[{"name": i, "id": i} for i in df.columns],
+        style_as_list_view=True,
+        style_cell={'padding': '5px'},
+        page_action='none',
+        style_table={'height': '800px','overflowY': 'auto'},
+        style_header={
+            'backgroundColor': 'white',
+            'fontWeight': 'bold',
+            'textAlign': 'center'
+        },
+        )
+    return prices
