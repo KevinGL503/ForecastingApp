795575c3fb95eb088a8119a2ee8d28b793777dcf
Kevin <abelgarcia612@gmail.com>
2024-06-09 11:19:28 -0500

    changes to updater script

--- api/updater_service.py
+++ api/updater_service.py
@@ -1,9 +1,11 @@
 """ Run this script to update stored conditions, prices, and forecasts"""
 from updater.db_helpers import DB
 from updater import forecast as fc
+from datetime import datetime
 
 def update():
     db = DB()
     db.update_curr_prices()
     db.update_curr_cond()
     fc.create_current_forecasts()
+    print(f"{datetime.now()}: Data has been updated")
---
+++ main_update.py
@@ -0,0 +1,4 @@
+import api.updater_service as ups
+
+if __name__ == '__main__':
+    ups.update()
